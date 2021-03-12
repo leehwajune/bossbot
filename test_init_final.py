74016251a1802886f671f4474e0f00e07248fad2
leehwajune <68924757+leehwajune@users.noreply.github.com>
2021-03-12 18:36:58 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1445,7 +1445,7 @@ class mainCog(commands.Cog):
 	async def setting_(self, ctx):	
 		#print (ctx.message.channel.id)
 		if ctx.message.channel.id == basicSetting[7]:
-			setting_val = '우그봇버전 : 기감보스 업글버전\n'
+			setting_val = '봇버전 : 기감보스 업글버전\n'
 			if basicSetting[6] != "" :
 				setting_val += '음성채널 : ' + self.bot.get_channel(basicSetting[6]).name + '\n'
 			setting_val += '텍스트채널 : ' + self.bot.get_channel(basicSetting[7]).name +'\n'
